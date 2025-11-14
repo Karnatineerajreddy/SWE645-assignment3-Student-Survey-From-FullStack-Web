f96c6c863b39e2a5b5426121fb88c5fd421679ce
Neeraj Reddy Karnati <neerajreddy974@gmail.com>
2025-11-14 01:23:16 -0500

    update surveylist.jsx

--- backend/app/main.py
+++ backend/app/main.py
@@ -33,7 +33,11 @@ app = FastAPI(title="Student Survey API")
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+    allow_origins=[
+        "http://100.30.1.131:31000",  # your frontend
+        "http://localhost:31000",
+        "http://127.0.0.1:31000",
+    ],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
